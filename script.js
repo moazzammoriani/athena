191783782899423001136e1c6bdb4a05b6163b32
Moazzam Moriani <mozmoriani@gmail.com>
2021-09-09 11:44:25 +0500

    Added a Responsive Carousel and Responsive Accordion Different media queries were added to make the carousel responsive. The accordions responsiveness was taken care of the by the page's own responsiveness.

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function toggleMobileMenu() {
         header.className = "";
     }
 }
+
+/*document.querySelector(".radio-container").addEventListener("change", (e) => {
+    // Selects the number of the input's index
+    inputIndex = e.target.getAttribute('id').slice(6);
+
+    firstSlide = document.querySelector('.first-slide');
+    
+    slideMotion = parseInt(inputIndex) * -100;
+    firstSlide.style.marginLeft = String(slideMotion) + "%";
+
+    
+})*/
+
+// Selecting all accordions
+accordions = document.querySelectorAll('.accordion');
+
+// Adding an event listener to all accordions, selecting their content and then toggling 
+// the the class that hides/displays them
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", (e) => {
+        accordion = e.target.parentNode;
+        for (let i = 0; i < accordion.childNodes.length; i++) {
+            if (accordion.childNodes[i].classList != undefined) {
+                if (accordion.childNodes[i].classList[0] == "accordion-content") {
+                    accordion.childNodes[i].classList.toggle("activate");
+                }
+            }
+        }
+    })
+}
