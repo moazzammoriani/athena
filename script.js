583e56b5d3486f01e5984320605b6d13fbbb68e6
Moazzam Moriani <mozmoriani@gmail.com>
2021-09-12 16:19:15 +0500

    Lots of changes

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const email = document.querySelector("#email");
 const subject = document.querySelector("#subject");
 const message = document.querySelector("#message");
 
-form.addEventListener("submit", (event) => {
+/*form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     email.addEventListener("input", () => {
@@ -68,4 +68,4 @@ form.addEventListener("submit", (event) => {
     successBanner.className += "success-banner";
     successBanner.innerHTML = `<i class="fas fa-check-circle"></i> Sumbission successful! We will get in touch with you soon.`
     formContainer.appendChild(successBanner);
-});
+});*/
